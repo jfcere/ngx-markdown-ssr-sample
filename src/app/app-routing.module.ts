5ada91c54a218be690ef4fa0684d29f91505c62f
jfcere <jfcere@sherweb.com>
2023-02-02 18:00:17 -0500

    Update Angular 14

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
+    initialNavigation: 'enabledBlocking'
 })],
   exports: [RouterModule]
 })
